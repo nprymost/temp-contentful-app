33c243e59bb6ac521adb7306868e254f8ab937c2
Natasha <natallia.prymost@gmail.com>
2023-11-07 13:05:14 -0800

    first commit

---
+++ src/Hero.jsx
@@ -0,0 +1,22 @@
+import heroImg from './assets/hero.svg';
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <div className="hero-title">
+          <h1>Contentful CMS</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+            hic iusto maxime, ipsum recusandae atque inventore obcaecati
+            architecto quidem adipisci.
+          </p>
+        </div>
+        <div className="img-container">
+          <img src={heroImg} alt="woman and the browser" className="img" />
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Hero;
